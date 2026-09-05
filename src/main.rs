c7c3e2f0b65b6d414fa5bc8b7c84b9758f325f2e
tim <tim7voetbal@live.nl>
2026-09-05 21:00:12 +0200

    added arguments to change path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use clap::Parser;
 use std::{
     fs::{read_dir, DirEntry},
     io::Result,
+    path::PathBuf,
 };
 
 mod duplication_pipeline;
@@ -8,14 +10,23 @@ mod hasher;
 mod image;
 mod image_reader;
 
+/// Program to determine multiple images with the same content.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The folder to search for images with the same content.
+    #[arg(short, long)]
+    folder: PathBuf,
+}
+
 fn main() -> Result<()> {
-    let directory_entries: Vec<Result<DirEntry>> =
-        read_dir("/mnt/hdd/_del/extHddData/anubis/output/0-20000/")?
-            .into_iter()
-            .collect();
+    let args = Args::parse();
+
+    let directory_entries: Vec<Result<DirEntry>> = read_dir(args.folder)?.into_iter().collect();
 
     let duplicates = duplication_pipeline::process_images(&directory_entries)?;
 
+    // TODO: Write to file
     for images in duplicates {
         for image in images {
             print!("\t{:?}", image.path);
